e6b9b4aca8527e993f0e783be9f7c13ec2aab946
Witrman <Vitr.man95@gmail.com>
2020-07-29 12:29:32 +0500

    Connectiong DB

--- Main.go
+++ Main.go
@@ -1,54 +1,87 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "log"
+  "context"
+  "fmt"
+  "log"
   //  "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-    //"go.mongodb.org/mongo-driver/mongo/readpref"
-  )
+  "go.mongodb.org/mongo-driver/mongo"
+  "go.mongodb.org/mongo-driver/mongo/options"
+  //"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+type autentification struct{
+  Access string
+  Refresh string
+}
 
-func Connect(){
+
+func main() {
+  client :=  Connect()
+
+}
+
+
+
+
+func Connect() mongo.client {
   // Create client
-  client, err := mongo.NewClient(options.Client().ApplyURI("witmanstas-shard-00-00.ei6tk.mongodb.net:27017"))
+  fmt.Println("Create client")
+  client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://user:user@clustervs.rwrgh.mongodb.net/BaseOne?retryWrites=true&w=majority"))
   if err != nil {
-      log.Fatal(err)
+    log.Fatal(err)
   }
+
 
   // Create connect
+  fmt.Println("Create connect")
   err = client.Connect(context.TODO())
   if err != nil {
-      log.Fatal(err)
+    log.Fatal(err)
   }
+
 
   // Check the connection
+  fmt.Println("Check the connection")
   err = client.Ping(context.TODO(), nil)
   if err != nil {
-      log.Fatal(err)
+    log.Fatal(err)
   }
+  fmt.Println("Connected to MongoDB Succesfull")
 
-  fmt.Println("Connected to MongoDB!")
+  return client
 
-  collection := client.Database("WitmanBase").Collection("AutCol")
+
+}
+
+func addData(){
+  user1 := autentification{"GhkjhsKd89DhjivsusHhfuidh9fvhu1","lilkdjfnlHLIUHerfgiwebllsdbLJDBLK9fvhu1"}
+  user2 := autentification{"GhkjhsKd89DhjivsusHhfuidh9fvhu2","lilkdjfnlHLIUHerfgiwebllsdbLJDBLK9fvhu2"}
+  user3 := autentification{"GhkjhsKd89DhjivsusHhfuidh9fvhu3","lilkdjfnlHLIUHerfgiwebllsdbLJDBLK9fvhu3"}
+
+  collection := client.Database("BaseOne").Collection("ACol")
   fmt.Println("Connected to Datebase and Collection!")
 
+  insertResult, err := collection.InsertOne(context.TODO(), user1)
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Println("Inserted a single document: ", insertResult.InsertedID)
 
+  users := []interface{}{user2, user3}
+  insertManyResult, err := collection.InsertMany(context.TODO(), users)
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Println("Inserted multiple documents: ", insertManyResult.InsertedIDs)
+
+  fmt.Println(collection.FindOne(context.TODO(), nil))
 }
 
 
 func close(){
-err = client.Disconnect(context.TODO())
-
-if err != nil {
+  err := client.Disconnect(context.TODO())
+  if err != nil {
     log.Fatal(err)
-}
-fmt.Println("Connection to MongoDB closed.")
-}
-
-func main() {
-  Connect()
-  time.Sleep(10 * time.Second)
-  close()
+  }
+  fmt.Println("Connection to MongoDB closed.")
 }

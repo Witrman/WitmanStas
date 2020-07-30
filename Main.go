e0f1f8ca63f392c3a32f9d8ce3d921bd15513ae1
Witrman <Vitr.man95@gmail.com>
2020-07-30 21:35:16 +0500

    heroku

--- Main.go
+++ Main.go
@@ -3,26 +3,28 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
-	"log"
-	"net/http"
-	//  "go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"net/http"
+	"time"
 	//"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
+/*
 type authentication struct {
-	GUID    string
-	Access  string
-	Refresh string
+	 GUID    string
+	 Access jwt.Token
+	 Refresh jwt.Token
 }
-
+*/
 var client *mongo.Client
 
 func main() {
 	ConnectDB()
-	addDataDB()
+	//addDataDB()
 	closeDB()
 	handlers()
 }
@@ -44,8 +46,9 @@ func ConnectDB() {
 	fmt.Println("Connected to MongoDB Successful")
 }
 
+/*
 func addDataDB() {
-	user1 := authentication{"Jdfjvnsskdjvns1", "GhkjhsKd89DhjivsusHhfuidh9fvhu1", "lilkdjfnlHLIUHerfgiwebllsdbLJDBLK9fvhu1"}
+	/*user1 := authentication{"Jdfjvnsskdjvns1", "GhkjhsKd89DhjivsusHhfuidh9fvhu1", "lilkdjfnlHLIUHerfgiwebllsdbLJDBLK9fvhu1"}
 	user2 := authentication{"Jdfjvnsskdjvns2", "GhkjhsKd89DhjivsusHhfuidh9fvhu2", "lilkdjfnlHLIUHerfgiwebllsdbLJDBLK9fvhu2"}
 	user3 := authentication{"Jdfjvnsskdjvns3", "GhkjhsKd89DhjivsusHhfuidh9fvhu3", "lilkdjfnlHLIUHerfgiwebllsdbLJDBLK9fvhu3"}
 
@@ -67,7 +70,7 @@ func addDataDB() {
 	session.EndSession(context.TODO())
 	fmt.Println("Documents dropped and added ")
 }
-
+*/
 func closeDB() {
 	errExc(client.Disconnect(context.TODO()))
 	fmt.Println("Connection to MongoDB closed.")
@@ -75,24 +78,40 @@ func closeDB() {
 
 func handlers() {
 	router := mux.NewRouter()
-	var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, err := w.Write([]byte("Not Implemented"))
+	var mySigningKey = []byte("WitmanStas")
+
+	var receive = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		AccessToken := jwt.New(jwt.SigningMethodHS512)
+		claims := make(jwt.MapClaims)
+		claims["admin"] = true
+		claims["name"] = "AccessToken"
+		claims["exp"] = time.Now().Add(time.Minute * 20).Unix()
+		AccessToken.Claims = claims
+		tokenString, _ := AccessToken.SignedString(mySigningKey)
+
+		_, err := w.Write([]byte(tokenString))
+		errExc(err)
+	})
+	var refresh = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("refrashhhh"))
+		errExc(err)
+	})
+	var del = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("deleted"))
+		errExc(err)
+	})
+	var clear = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("clearing"))
 		errExc(err)
 	})
 
-	router.Handle("/receive", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, err := w.Write([]byte("Recieve"))
-		errExc(err)
-	})).Methods("Get")
+	router.Handle("/receive", receive).Methods("Get")
 
-	router.Handle("/refresh", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, err := w.Write([]byte("refrashhhh"))
-		errExc(err)
-	})).Methods("Get")
+	router.Handle("/refresh", refresh).Methods("Get")
 
-	router.Handle("/delete", NotImplemented).Methods("Get")
+	router.Handle("/delete", del).Methods("Get")
 
-	router.Handle("/clear", NotImplemented).Methods("Get")
+	router.Handle("/clear", clear).Methods("Get")
 
 	errExc(http.ListenAndServe(":3000", router))
 
